b9c1af783703dea886482f304aa02117fc8a7b25
Maria Talan <mashenka.talan@gmail.com>
2023-04-17 17:45:20 +0300

    Bug fixed

--- js/task-02.js
+++ js/task-02.js
@@ -8,15 +8,15 @@ const ingredients = [
 ];
 
 const ingredientsListEl = document.querySelector('#ingredients');
+const fragment = document.createDocumentFragment();
 
 for (const ingredient of ingredients) {
   const ingredientEl = document.createElement('li');
-
   ingredientEl.textContent = ingredient;
   ingredientEl.classList.add('item');
+  fragment.appendChild(ingredientEl);
 
-  ingredientsListEl.appendChild(ingredientEl);
 }
 
-
+ingredientsListEl.appendChild(fragment);
 
--- js/task-08.js
+++ js/task-08.js
@@ -7,21 +7,18 @@ function onFormSubmit(event) {
   event.preventDefault();
 
   for (const element of event.currentTarget.elements) {
-    const { name, value, type } = element;
-    console.log('current element', { name, value, type })
+    const {name, value, type} = element;
     if (value === '' && type !== 'submit') {
       alert('Все поля должны быть заполнены!!!!');
-      return;
-    }
-    if (type !== 'submit') {
-      console.log('name', name)
+      break;
+    } else {
       data[name] = value;
     }
   }
 
   console.log(data);
-
   form.reset();
 
 }
+
 
