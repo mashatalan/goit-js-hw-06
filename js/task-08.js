8a4a6f6ab94dd556cd2915ee787cffe01444a514
Maria Talan <mashenka.talan@gmail.com>
2023-04-17 21:46:05 +0300

    Applied corrections

--- js/task-08.js
+++ js/task-08.js
@@ -1,24 +1,22 @@
 const form = document.querySelector('.login-form');
-const data = {};
+
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  for (const element of event.currentTarget.elements) {
-    const {name, value, type} = element;
-    if (value === '' && type !== 'submit') {
-      alert('Все поля должны быть заполнены!!!!');
-      break;
-    } else {
-      data[name] = value;
-    }
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Все поля должны быть заполнены');
+    return;
   }
 
+  const data = { email, password };
   console.log(data);
   form.reset();
-
 }
 
 
